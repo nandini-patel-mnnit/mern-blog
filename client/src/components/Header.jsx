c129407f1a95c788703a38ce4ba7dc7bd9349dda
nandini-patel-mnnit <nandinipatel01012004@gmail.com>
2024-06-12 17:35:01 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Flowbite } from 'flowbite-react';
 import { Button, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
@@ -11,7 +10,7 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-yellow-400 via-red-600 to-indigo-500 rounded-lg text-white'>Sahand's</span>
+        <span className='px-2 py-1 bg-gradient-to-r from-yellow-400 via-orange-500 to-red-700 rounded-lg text-white'>Sahand's</span>
         Blog
       </Link>
       <form>
@@ -30,29 +29,29 @@ export default function Header() {
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='tealToLime' outline>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle/>
       </div>
       <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={'div'}>
-            <Link to='/'>
-              Home
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/about"} as={'div'}>
-            <Link to='/about'>
-              About
-            </Link>
-          </Navbar.Link >
-          <Navbar.Link active={path === "/projects"} as={'div'}>
-            <Link to='/projects'>
-              Projects
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={'div'}>
+          <Link to='/'>
+            Home
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={'div'}>
+          <Link to='/about'>
+            About
+          </Link>
+        </Navbar.Link >
+        <Navbar.Link active={path === "/projects"} as={'div'}>
+          <Link to='/projects'>
+            Projects
+          </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
