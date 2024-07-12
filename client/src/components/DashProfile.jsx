aac90905fedb8cd70c49b6262198a92a4f8f3daf
nandini-patel-mnnit <nandinipatel01012004@gmail.com>
2024-07-12 22:12:33 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,11 @@
 import { useState, useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { TextInput, Button, Alert, Modal } from 'flowbite-react';
+import { TextInput, Button, Alert, Modal, ModalBody } from 'flowbite-react';
 import { app } from '../firebase';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
@@ -124,6 +124,24 @@ export default function DashProfile() {
     }
   };
 
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method : 'POST',
+      });
+      const data = await res.json();
+      if(!res.ok) {
+        console.log(data.message);
+      }
+      else {
+        dispatch(signoutSuccess());
+      }
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full ">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -175,7 +193,7 @@ export default function DashProfile() {
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
           Delete Account
         </span>
-        <span className="cursor-pointer">
+        <span onClick={handleSignout} className="cursor-pointer">
           Sign Out
         </span>
       </div>
