c95492d8878363ee9b613e29345cc1d07df78581
nandini-patel-mnnit <nandinipatel01012004@gmail.com>
2024-07-12 13:29:27 +0530

    complete update user profile page functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -4,14 +4,14 @@ const initialState = {
     currentUser : null,
     error : null,
     loading : false
-}
+};
 
 const userSlice = createSlice({
     name : 'user',
     initialState,
     reducers : {
         signInStart : (state) => {
-            state.Loading = true;
+            state.loading = true;
             state.error = null;
         },
         signInSuccess : (state, action) => {
@@ -23,9 +23,22 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        updateStart : (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        updateSuccess : (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateFailure : (state, action) => {
+            state.loading  = false;
+            state.error = action.payload;
+        },
     },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure } = userSlice.actions;
 
 export default userSlice.reducer;
