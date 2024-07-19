5f04d5abe46fcc56de91ac9e7a95f8d3d7ee4f2f
nandini-patel-mnnit <nandinipatel01012004@gmail.com>
2024-07-19 16:44:47 +0530

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi';
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation, HiChartPie } from 'react-icons/hi';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
@@ -41,6 +41,15 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {
+            currentUser && currentUser.isAdmin && (
+              <Link to='/dashboard?tab=dash'>
+                <Sidebar.Item active={tab === 'dash' || !tab} icon={HiChartPie} as='div' >
+                  Dashboard
+                </Sidebar.Item>
+              </Link>
+            )
+          }
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? "Admin" : "User"} labelColor='dark' as='div'>
               Profile
@@ -49,7 +58,7 @@ export default function DashSidebar() {
           {
             currentUser.isAdmin && (
             <Link to='/dashboard?tab=posts'>
-              <Sidebar.Item onClick={() => console.log('Hello')} active={tab === 'posts'} icon={HiDocumentText} as='div' >
+              <Sidebar.Item active={tab === 'posts'} icon={HiDocumentText} as='div' >
                 Posts
               </Sidebar.Item>
             </Link>)
@@ -58,12 +67,12 @@ export default function DashSidebar() {
             currentUser.isAdmin && (
               <>
                 <Link to='/dashboard?tab=users'>
-                  <Sidebar.Item onClick={() => console.log('Hello')} active={tab === 'users'} icon={HiOutlineUserGroup} as='div' >
+                  <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div' >
                     Users
                   </Sidebar.Item>
                 </Link>
                 <Link to='/dashboard?tab=comments'>
-                  <Sidebar.Item onClick={() => console.log('Hello')} active={tab === 'comments'} icon={HiAnnotation} as='div' >
+                  <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div' >
                     Comments
                   </Sidebar.Item>
                 </Link>
