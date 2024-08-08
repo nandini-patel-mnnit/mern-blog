2a1eb441ee02ec92f872f6243a354dd7359095bf
nandini-patel-mnnit <nandinipatel01012004@gmail.com>
2024-08-09 00:02:19 +0530

    made some changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className='min-h-screen flex items-center justify-center'>
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
-          <h1 classname='text-3xl font font-semibold text-center my-7'>About Sahand's Blog</h1>
+          <h1 classname='text-3xl font font-semibold text-center my-7'>About Nandini's Blog</h1>
           <div className='text-md text-gray-500 flex flex-col gap-6 '>
             <p>
               Welcome to Nandini's Blog! This blog was created by Nandini Patel as a personal project to share his thoughts and ideas with the world. Nandini is a passionate developer who loves to write about technology, coding, and everything in between.
